6abb467cd2ad7780a28cf86e1ded33fdfcb57fb1
Vadim Dvorovenko <Vadimon@mail.ru>
2020-06-13 10:36:04 +0700

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_quickrenamequestioncategories';
-$plugin->version   = 2016051000;
-$plugin->release = '1.0.1';
+$plugin->version   = 2020061200;
+$plugin->release = '1.1';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires  = 2015111600; // Moodle 3.0.
