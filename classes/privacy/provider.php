bfbe6b4544b2c11fb56c35a6e957c4ffc58441cb
Vadim Dvorovenko <Vadimon@mail.ru>
2025-01-28 23:52:26 +0700

    remove legacy_polyfill from privacy provider

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -33,15 +33,13 @@ namespace qbank_quickrenamecategories\privacy;
  */
 class provider implements \core_privacy\local\metadata\null_provider {
 
-    use \core_privacy\local\legacy_polyfill;
-
     /**
      * Get the language string identifier with the component's language
      * file to explain why this plugin stores no data.
      *
      * @return  string
      */
-    public static function _get_reason() {
+    public static function get_reason(): string {
         return 'privacy:metadata';
     }
 }
