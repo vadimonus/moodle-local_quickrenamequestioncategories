f6066e460fcda190659433ab496ba10b8690f785
Vadim Dvorovenko <Vadimon@mail.ru>
2025-02-23 11:53:50 +0700

    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbank_quickrenamecategories';
-$plugin->version   = 2025022301;
+$plugin->version   = 2025022302;
 $plugin->release = '3.0';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires  = 2024100700; // Moodle 4.5.
